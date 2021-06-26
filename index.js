b8629e09b8204ec14a08c19a885ebfff5f6ba3e0
ANIJITH AM <56534464+anijithanil@users.noreply.github.com>
2021-06-26 20:11:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Info from "./components/Info";
+
 
 ReactDOM.render(
   <div>
